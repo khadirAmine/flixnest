d64c253cc705c46f3a7f6ca00ca635bee19f0004
khadir <mohammedaminekhadir6@gmail.com>
2024-11-08 21:28:15 +0100

    update splash screen

--- lib/core/config/assets.dart
+++ lib/core/config/assets.dart
@@ -1,3 +1,4 @@
 class Assets {
   static const String errorImage = 'assets/images/error_image.jpg';
+  static const String logo = 'assets/images/logo.png';
 }
--- lib/view/screen/splash_screen.dart
+++ lib/view/screen/splash_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
-import 'package:visibility_detector/visibility_detector.dart';
 
 import '../../controller/home_controller.dart';
+import '../../core/config/assets.dart';
 import '../../core/config/routes.dart';
 import '../../core/config/theme.dart';
 import '../../core/service/scrapping_service.dart';
@@ -10,20 +11,37 @@ import '../../core/service/scrapping_service.dart';
 class SplashScreen extends StatelessWidget {
   SplashScreen({super.key});
   final SplashScreenController spController = Get.put(SplashScreenController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: AppTheme().instance.theme.splashColor,
-        body: Center(
-          child: VisibilityDetector(
-              key: const Key('myKey'),
-              onVisibilityChanged: (visibilityInfo) {
-                var visiblePercentage = visibilityInfo.visibleFraction * 100;
-                debugPrint(
-                    'Widget ${visibilityInfo.key} is ${visiblePercentage}% visible');
-              },
-              child: Container(width: 100, height: 100, color: Colors.red)),
-        ));
+      body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                AppTheme().instance.theme.primaryColor.withAlpha(150),
+                AppTheme().instance.theme.primaryColor,
+              ])),
+          child: Center(
+            child: SizedBox(
+                width: Get.width * 0.3,
+                height: Get.height * 0.1425,
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                        child: Image.asset(
+                      Assets.logo,
+                    )),
+                    SpinKitDualRing(
+                      color: AppTheme().instance.theme.colorScheme.secondary,
+                      size: 60.0,
+                    ),
+                  ],
+                )),
+          )),
+    );
   }
 }
 
